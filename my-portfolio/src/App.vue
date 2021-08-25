49bd7449d053334b70ff5a4b0140579f36f39b86
佐藤魁 <satokai@satoukainoMBP.AirPort>
2021-08-25 14:29:34 +0900

    vueに移行

---
+++ my-portfolio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <v-app id="inspire" dark>
+      <router-view/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #1b1d21;
+  height: 100%;
+  padding: 0 300px;
+}
+</style>
---
+++ my-portfolio/src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class='main'>
+    <div class='prof'>
+      <img src="../assets/my_icon.png" class='my-icon'>
+      <h1>kaaai</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  color: aliceblue;
+}
+.main {
+  background-color: #19171d;
+  /* background-color: #000; */
+  height: 100%;
+}
+.prof {
+  margin-top: 200px;
+}
+.my-icon {
+  width: 15%;
+}
+</style>
